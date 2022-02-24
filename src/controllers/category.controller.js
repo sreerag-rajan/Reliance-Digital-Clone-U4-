b38234fef0113b27aabb4c7d5ce9ffdb8d050c14
MayuR397 <mayurkhachane67@gmail.com>
2022-02-24 12:50:26 +0530

    worked on post patch Del will start adding it to all sections

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,17 +2,51 @@ const express = require("express");
 const Category = require("../models/category.model");
 const Product = require("../models/product.model");
 const router = express.Router();
+const app = express();
+app.use(express.json());
 
-router.get("/:category", async(req, res)=>{
-    try{
-        const category = await Category.findOne({name:req.params.category}).lean().exec();
-        
+router.post("/category", async (req, res) => {
+  try {
+    console.log(req.body);
+    const category = await Category.create(req.body);
 
+    return res.status(201).send(category);
+  } catch (error) {
+    console.log("error:", error);
+  }
+});
+
+router.patch("/category/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const category = await Category.findByIdAndUpdate(_id, req.body);
+        res.send(category)
+        console.log(_id, req.body);
+    } catch (error) {
+        console.log('error:', error);
+        res.status(400).send(error);
     }
-    catch(er){
-        return res.status(500).send(er.message)
+});
+
+router.delete("/category/:id", async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+        res.send(category)
+        
+    } catch (error) {
+        console.log('error:', error);
+        res.status(400).send(error);
     }
 })
 
+router.get("/:category", async (req, res) => {
+  try {
+    const category = await Category.findOne({ name: req.params.category })
+      .lean()
+      .exec();
+  } catch (er) {
+    return res.status(500).send(er.message);
+  }
+});
 
 module.exports = router;
