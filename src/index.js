8df916f4acc4bd935e4f82d6a5d70e69031e2062
iamrituyadav <iamrituyadav@outlook.com>
2022-02-24 14:18:53 +0530

    added authentication functionality

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const connect = require("./configs/db");
-const searchController = require("./controllers/search.controller");
+// const searchController = require("./controllers/search.controller");
 
+const userController = require("./controllers/user.controller");
+const { register, login } = require("./controllers/auth.controller");
 
-app.post("/search",searchController);
+// app.post("/search", searchController);
 
+app.post("/register", register);
+app.post("/login", login);
 
+app.use("/users", userController);
 
-app.listen(2345,  async ()=>{
-    try{
-        await connect();
-        console.log("listening on port 2345")
-
-    }
-    catch(err){
-        console.log(err.message)
-    }
-    
+app.listen(2345, async () => {
+  try {
+    await connect();
+    console.log("listening on port 2345");
+  } catch (err) {
+    console.log(err.message);
+  }
 });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,16 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    firstName:{type:String,required:true},
-    lastName:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    phone:{type:String,required:true},
-    address:[{type:String,required:false}],
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: {
+      type: ["customer", "seller", "Admin"],
+      required: false,
+      default: "customer",
+    },
+    address: [{ type: String, required: false }],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-},{
-    versionKey:false,
-    timestamps:true,
-})
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
 
-module.exports = mongoose.model("user",userSchema); //mongo collection --> users
+  var hash = bcrypt.hashSync(this.password, 8);
+  this.password = hash;
+  return next();
+});
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
