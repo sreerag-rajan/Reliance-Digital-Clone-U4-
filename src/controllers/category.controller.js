542e62c7e780fe4031f06c04c472c20f04a06c98
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-02-27 16:59:09 +0530

    laptops linked to its page

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -34,7 +34,7 @@ router.get("/:category", async(req, res)=>{
     try{
         
         const category = await Category.findOne({name:req.params.category}).lean().exec();
-        console.log(category)
+        // console.log(category)
         const images = await Images.find({category:category._id}).lean().exec()
 
         const prodrows = await Productrow.find({category:category._id}).populate({path:"products", select:["name","price","MRP","discount","main_img"]}).lean().exec()
@@ -43,7 +43,7 @@ router.get("/:category", async(req, res)=>{
             images,
             prodrows
         }
-        console.log(images);
+        // console.log(images);
         return res.status(201).render("productcatalogfromnav.ejs",content);
 
 
